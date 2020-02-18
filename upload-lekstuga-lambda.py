11b57767f140856bb770782179fb31b472b3bd6b
Johan Hanses <johanhanses@gmail.com>
2020-02-18 11:54:11 +0100

    converted script to a lambda function

--- upload-lekstuga-lambda.py
+++ upload-lekstuga-lambda.py
@@ -1,18 +1,27 @@
+import json
 import boto3
 import StringIO
 import zipfile
 import mimetypes
 
-s3 = boto3.resource("s3")
+def lambda_handler(event, context):
+    s3 = boto3.resource("s3")
 
-lekstuge_bucket = s3.Bucket("lekstuga.johanhanses.net")
-bygge_bucket = s3.Bucket("lekstugebygge.johanhanses.net")
+    lekstuge_bucket = s3.Bucket("lekstuga.johanhanses.net")
+    bygge_bucket = s3.Bucket("lekstugebygge.johanhanses.net")
+    
+    lekstuge_zip = StringIO.StringIO()
+    bygge_bucket.download_fileobj("lekstugebygge", lekstuge_zip)
+    
+    with zipfile.ZipFile(lekstuge_zip) as myzip:
+        for nm in myzip.namelist():
+            obj = myzip.open(nm)
+            lekstuge_bucket.upload_fileobj(obj, nm, ExtraArgs={"ContentType": mimetypes.guess_type(nm)[0]})
+            lekstuge_bucket.Object(nm).Acl().put(ACL="public-read")
+    
+    print("Job done!")
 
-lekstuge_zip = StringIO.StringIO()
-bygge_bucket.download_fileobj("lekstugebygge", lekstuge_zip)
-
-with zipfile.ZipFile(lekstuge_zip) as myzip:
-    for nm in myzip.namelist():
-        obj = myzip.open(nm)
-        lekstuge_bucket.upload_fileobj(obj, nm, ExtraArgs={"ContentType": mimetypes.guess_type(nm)[0]})
-        lekstuge_bucket.Object(nm).Acl().put(ACL="public-read")
+    return {
+        'statusCode': 200,
+        'body': json.dumps('Hello from Lambda!')
+    }
