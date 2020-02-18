49ab6f14a77f03cd79aa0d2b232bc5af808fe8c1
Johan Hanses <johanhanses@gmail.com>
2020-02-18 11:21:23 +0100

    added a upload to s3-script

---
+++ upload-lekstuga-lambda.py
@@ -0,0 +1,18 @@
+import boto3
+import StringIO
+import zipfile
+import mimetypes
+
+s3 = boto3.resource("s3")
+
+lekstuge_bucket = s3.Bucket("lekstuga.johanhanses.net")
+bygge_bucket = s3.Bucket("lekstugebygge.johanhanses.net")
+
+lekstuge_zip = StringIO.StringIO()
+bygge_bucket.download_fileobj("lekstugebygge", lekstuge_zip)
+
+with zipfile.ZipFile(lekstuge_zip) as myzip:
+    for nm in myzip.namelist():
+        obj = myzip.open(nm)
+        lekstuge_bucket.upload_fileobj(obj, nm, ExtraArgs={"ContentType": mimetypes.guess_type(nm)[0]})
+        lekstuge_bucket.Object(nm).Acl().put(ACL="public-read")
